4af76c330bec5bc61a68a0441e76dd5d6ada7b34
heitorHR oliveira <heitor.1213oliveira@gmail.com>
2024-06-04 01:50:51 +0000

    alteração dinâmica do assunto da pagina

--- pages/resultados/resultado.js
+++ pages/resultados/resultado.js
@@ -4,7 +4,7 @@ import { verificarTema, trocarTema} from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body") 
-botaoTema.addEventListener("click", ()=> {
+botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
